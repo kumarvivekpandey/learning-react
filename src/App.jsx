c41f8037b16ce8f0454aa484ea428e6d1bc40f99
Kumarvivekpandey rock <>
2023-02-19 18:01:57 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+export default function App() {
+  return (
+    <main>
+      React ⚛️ + Vite ⚡ + Replit 🌀
+      <h1> hello  vivek how are you </h1>
+      <p> hii i am a paragraph</p>
+     <h2> const a=22,
+      let b= 34
+      console.log(b);</h2>
+    </main>
+  )
+}
